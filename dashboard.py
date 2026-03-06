3a945c3bb4b0bc327099a32e3616628e68c73179
tomasola <tomas3050@gmail.com>
2026-03-06 23:21:12 +0100

    Fix: change SocketIO async_mode to gevent

--- dashboard.py
+++ dashboard.py
@@ -61,7 +61,7 @@ app = Flask(__name__)
 app.config['TEMPLATES_AUTO_RELOAD'] = True
 app.config['SEND_FILE_MAX_AGE_DEFAULT'] = 0
 
-socketio = SocketIO(app, cors_allowed_origins="*", async_mode='eventlet')
+socketio = SocketIO(app, cors_allowed_origins="*", async_mode='gevent')
 
 @app.after_request
 def add_header(response):
